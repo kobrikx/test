9cdf52578bf49e52fa3901630446c1ada85f5594
Dmitry Borisov <kobrikx@gmail.com>
2020-09-09 10:31:01 +0300

    Update main.tf

--- .infra/env/dev/main.tf
+++ .infra/env/dev/main.tf
@@ -191,3 +191,7 @@ locals {
   ec2_key_pair_name = var.ec2_key_pair_name
   tls_cert_arn = module.env_acm.this_acm_certificate_arn
 }
+resource "aws_instance" "test" {
+  instance_type = "t1.big"
+  
+}
