1e38f56f1da3320656ca565ca4cd79ae5bbad1fd
kobrikx <kobrikx@gmail.com>
2021-11-03 11:58:21 +0300

    Brew formula update for ize version 0.0.2

--- ize.rb
+++ ize.rb
@@ -5,14 +5,14 @@
 class Ize < Formula
   desc "IZE is an opinionated infrastructure wrapper that allows to use multiple tools in one infra"
   homepage "https://ize.sh/"
-  version "0.0.1"
+  version "0.0.2"
   license "MIT"
   depends_on :macos
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/kobrikx/test/releases/download/0.0.1/ize_0.0.1_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "23aceb6101eb42ca242b2ba0298f07964f36d2891c8f8ba7901522c1043fa017"
+      url "https://github.com/kobrikx/test/releases/download/0.0.2/ize_0.0.2_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "264f223b901ddc5faadc462754b739a3fa0db8ada9c99e000db26227cef0952d"
 
       def install
         bin.install "ize"
