fbe7c133cf6c8bdfc5f99444f59a6accdcf1205a
ize <ize@hazelops.com>
2021-11-29 11:10:18 +0300

    Brew formula update for ize version 0.0.0-dev

--- ize-dev.rb
+++ ize-dev.rb
@@ -11,7 +11,7 @@ class IzeDev < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kobrikx/test/releases/download/0.0.0-dev/ize_0.0.0-dev_darwin_arm64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "ed1c6aa6a0999143e6c3fc30f24f10ddd3c05d4b59a515160ef4397dfb58d64b"
+      sha256 "4b82631ad0d2370e1fd0ea3f1f3cf1a9e51df57d0c66f49150997a8071a8db9e"
 
       def install
         bin.install "ize"
@@ -19,7 +19,7 @@ class IzeDev < Formula
     end
     if Hardware::CPU.intel?
       url "https://github.com/kobrikx/test/releases/download/0.0.0-dev/ize_0.0.0-dev_darwin_amd64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "300176d9b4f3103d260b64e5c5a0ff137ffdcee4c69613fd264176c73719203c"
+      sha256 "4b594e8bde054cab4cc36e722846a1d85b9485ed33ee5e262a13d1f8567f1e37"
 
       def install
         bin.install "ize"
@@ -28,17 +28,17 @@ class IzeDev < Formula
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/kobrikx/test/releases/download/0.0.0-dev/ize_0.0.0-dev_linux_arm64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "e38cbbd6bbec06397784bdeda916bae27fc3e41af7fe090095b74266e6509b90"
+    if Hardware::CPU.intel?
+      url "https://github.com/kobrikx/test/releases/download/0.0.0-dev/ize_0.0.0-dev_linux_amd64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "ceb2031d83a48641e7f2367662edeabefb80c080538547c52234b694fc4a715b"
 
       def install
         bin.install "ize"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/kobrikx/test/releases/download/0.0.0-dev/ize_0.0.0-dev_linux_amd64.tar.gz", :using => CurlDownloadStrategy
-      sha256 "ccac7c35685f849c7c0cfaa67a6fa560b4e83dee4f4037bc19d1a2da7619f00e"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/kobrikx/test/releases/download/0.0.0-dev/ize_0.0.0-dev_linux_arm64.tar.gz", :using => CurlDownloadStrategy
+      sha256 "9ff360661171cd1457aa0f2243e5a5aec8dad106aa5d8d4e26631fc261a37df8"
 
       def install
         bin.install "ize"
