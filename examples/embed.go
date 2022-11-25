b9a8302d70cf15b76b19d7fdb9aea36f4e0a3e10
Dmitry Borisov <dmitry.borisov@hazelops.com>
2022-11-25 14:21:38 +0300

    test1

--- examples/embed.go
+++
@@ -1,8 +0,0 @@
-package examples
-
-import "embed"
-
-var (
-	//go:embed */*
-	Examples embed.FS
-)
--- internal/apps/interface.go
+++
@@ -1,8 +0,0 @@
-package apps
-
-import "github.com/hazelops/ize/pkg/terminal"
-
-type Deployment interface {
-	Deploy(sg terminal.StepGroup, ui terminal.UI) error
-	Destroy(sg terminal.StepGroup, ui terminal.UI) error
-}
